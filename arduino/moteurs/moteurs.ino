c495a13d08d2ec20ebea260b71843138fec5c594
Younousse Soufiani <soufiani08@live.fr>
2022-10-03 21:33:39 +0200

    prise d'info sur la vitesse de rotation moteur

--- arduino/moteurs/moteurs.ino
+++ arduino/moteurs/moteurs.ino
@@ -1,7 +1,15 @@
-int ENB=9;
+#include<TimerOne.h>
+int ENB=11;
 int IN3=6;
 int IN4=7;
+float vitesseD;
+float Mult=(3.14*6)/(1920*150000*0.000001);
+long compteurD;
 void setup() {
+  Timer1.initialize(150000);
+  attachInterrupt(0,changeD,RISING);
+  Timer1.attachInterrupt(CalculVitesseD);
+  Serial.begin(9600);
   pinMode(ENB,OUTPUT);
   pinMode(IN3,OUTPUT);
   pinMode(IN4,OUTPUT);
@@ -11,9 +19,16 @@ void setup() {
   digitalWrite(IN3,LOW);
   digitalWrite(IN4,HIGH);
 }
+void changeD(){compteurD++;}
+
+void CalculVitesseD(void){ 
+  vitesseD=Mult*compteurD;
+ //   Serial.println(compteurD);
+ Serial.println(vitesseD,2);
+  compteurD=0;}
+
 
 void loop() {
   //plein régime
-  analogWrite(ENB,63);
-
+  analogWrite(ENB,255);
 }
