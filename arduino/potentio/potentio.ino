e99c1f5e45c9ff4778efbffb8dc87692fc70ea51
Younousse Soufiani <soufiani08@live.fr>
2022-10-19 10:36:55 +0200

    cours CAO

--- arduino/potentio/potentio.ino
+++ arduino/potentio/potentio.ino
@@ -8,19 +8,19 @@ Serial.begin(9600);
 }
 
 void loop() {
-//  Serial.println(potentio);
-//  potentio=analogRead(0);
-//  tension=potentio/204,6 ;
-//  Serial.print("tension= ");
-//  Serial.println( tension);
-//  dio= tension*var;
-//  analogWrite(3,dio);
-//  delay(500);
+  Serial.println(potentio);
+  potentio=analogRead(0);
+  tension=potentio/204,6 ;
+  Serial.print("tension= ");
+  Serial.println( tension);
+  dio= tension*var;
+  analogWrite(3,dio);
+  delay(500);
 
-  for(i=0;i<=255;i++){
-    analogWrite(3,i);
-    delay(10);
-  }
+//  for(i=0;i<=255;i++){
+//    analogWrite(3,i);
+//    delay(10);
+//  }
     
   
 }
